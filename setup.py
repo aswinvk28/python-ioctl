473244307585c46856d403b775fe318d5051f85d
Aswin Vijayakumar <aswinvk28@gmail.com>
2023-02-27 10:27:53 +0000

    Update setup.py
    Added vexf package requiring an ioctl helper function.

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ if sys.version_info < (3, 3):
     tests_require.append('mock') # mock became available as unittest.mock from Python 3.3.
 
 setup(
-    name = 'ioctl',
+    name = 'vexf_ioctl',
     packages = find_packages(),
     use_scm_version = True,
-    description = 'ioctl helper functions',
+    description = 'ioctl helper functions for vexf (vision exchange format)',
     license = 'MIT',
-    maintainer = 'Olav Morken',
-    maintainer_email = 'olavmrk@gmail.com',
+    maintainer = 'Aswin Vijayakumar',
+    maintainer_email = 'aswinvk28@gmail.com',
     url = 'https://github.com/olavmrk/python-ioctl',
-    download_url = 'https://github.com/olavmrk/python-ioctl/releases',
+    download_url = 'https://github.com/aswinvk28/python-ioctl/releases',
     setup_requires = [ 'setuptools_scm' ],
     tests_require = tests_require,
     test_suite = 'tests',
